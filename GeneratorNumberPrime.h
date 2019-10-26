340441186046edc4a79ecb1e66c8eb7f2e2a737a
higor <higor321@hotmail.com>
2019-10-26 10:22:21 -0300

    THREADS PRONTA

--- GeneratorNumberPrime.h
+++ GeneratorNumberPrime.h
@@ -1,15 +1,8 @@
 #ifndef GeneratorNumberPrime_h
 #define GeneratorNumberPrime_h
-#define TRUE 1
-#define FALSE 0
+#include "Options.h"
 
 // FUNÇÃO QUE GERA NUMEROS PRIMOS ENTRE UM INTERVALO
-void *GeneratorNumberPrime ( unsigned long, unsigned long );
-
-// VERIFICA SE É PRIMO
-int isPrime( unsigned long);
-
-//VERSÃO THREAD
-void GeneratorNumberPrimeThread ( unsigned long, unsigned long );
+void GeneratorNumberPrime ( unsigned long, unsigned long, Options);
 
 #endif
