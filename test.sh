a8da5e25ba84e9ca3101ef084563d380a5bd4874
higor <higor321@hotmail.com>
2019-10-28 15:54:51 -0300

    VERSAO FINAL

---
+++ test.sh
@@ -0,0 +1,20 @@
+echo ./gnp 0 1000000 0 1
+./gnp 0 1000000 0 1 > results.txt
+echo ./gnp 0 1000000 1 1
+./gnp 0 1000000 1 1 > results.txt
+echo ./gnp 0 1000000 2 1
+./gnp 0 1000000 2 1 > results.txt
+
+echo ./gnp 0 10000000 0 1
+./gnp 0 10000000 0 1 > results.txt
+echo ./gnp 0 10000000 1 1
+./gnp 0 10000000 1 1 > results.txt
+echo ./gnp 0 10000000 2 1
+./gnp 0 10000000 2 1 > results.txt
+
+echo ./gnp 0 1000000000 0 1
+./gnp 0 100000000 0 1 > results.txt
+echo ./gnp 0 1000000000 1 1
+./gnp 0 100000000 1 1 > results.txt
+echo ./gnp 0 1000000000 2 1
+./gnp 0 100000000 2 1 > results.txt
